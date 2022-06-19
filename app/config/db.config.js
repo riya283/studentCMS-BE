1afb032ca2c343fc6885262bfd01ba38b1d4116f
Riya Patel <riyapatel7021@gmail.com>
2022-06-19 14:28:39 +0530

    heroku db configuratiin

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "studentCMS",
+    HOST: "ec2-34-194-158-176.compute-1.amazonaws.com",
+    USER: "lieequaxmnrghh",
+    PASSWORD: "99665986357d1e2f11be0f57c9bbde445df2087509b2963948897795e9324640",
+    DB: "dagiouneoeg0o9",
     dialect: "postgres",
     pool: {
       max: 5,
