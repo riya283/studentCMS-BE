d8226461b1792c5b8097df30cd72abc860635fb9
Riya Patel <riyapatel7021@gmail.com>
2022-06-19 14:50:41 +0530

    try 1

--- app/models/index.js
+++ app/models/index.js
@@ -1,18 +1,25 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
+//   dialectOptions: {
+//     ssl: true
+//   },
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   }
+// });
+
+var sequelize = new Sequelize("postgres://lieequaxmnrghh:99665986357d1e2f11be0f57c9bbde445df2087509b2963948897795e9324640@ec2-34-194-158-176.compute-1.amazonaws.com:5432/dagiouneoeg0o9", {
+  dialect: 'postgres',
   dialectOptions: {
     ssl: true
-  },
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
   }
 });
 
